000c9ac86981e2f2b14005c853d5ae58a330abeb
Jussi <Jussi@DESKTOP-H40GER5>
2017-02-22 20:25:40 +0200

    Teht01

---
+++ Demo06_1/teht01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give text (enter ends) : ");
+            string teksti = Console.ReadLine();
+
+            System.IO.StreamWriter outputFile = new System.IO.StreamWriter("test.txt");
+            outputFile.WriteLine(teksti);
+            outputFile.Close();
+
+            string text = System.IO.File.ReadAllText("test.txt");
+            System.Console.WriteLine("Contents of test.txt: \n" + text);
+        }
+    }
+}
